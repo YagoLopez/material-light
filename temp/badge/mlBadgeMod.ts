b2da51c8697f4a58d46c9793a760794668ed1e3e
YagoLopez <yago.lopez@gmail.com>
2017-06-07 13:07:07 +0200

    Bug fix: badge + icon bad positioned by css

--- temp/badge/mlBadgeMod.ts
+++
@@ -1,4 +0,0 @@
-import {NgModule} from "@angular/core";
-import {MlBadge} from "./mlBadge";
-import {MlIconMod} from "../icon/mlIconMod";
-@NgModule({declarations: [MlBadge], exports: [MlIconMod, MlBadge]}) export class MlBadgeMod{}
